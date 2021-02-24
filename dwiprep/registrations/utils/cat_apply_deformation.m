9f3896b714a58dacaf3587945a96d124f2317748
Gal Hershkovitz <hershkovitz1@mail.tau.ac.il>
2021-02-24 14:28:25 +0200

    both FSL and CAT-derived deformations are now applied to parcellations if given by the user

--- dwiprep/registrations/utils/cat_apply_deformation.m
+++ dwiprep/registrations/utils/cat_apply_deformation.m
@@ -7,9 +7,13 @@ function cat_apply_deformation(field_file,in_file,interp)
 
 spm('defaults', 'fmri');
 spm_jobman('initcfg');
-matlabbatch{1}.spm.tools.cat.tools.defs2.field = {char(field_file+",1")};
-matlabbatch{1}.spm.tools.cat.tools.defs2.images = {{char(in_file+",1")}};
-matlabbatch{1}.spm.tools.cat.tools.defs2.interp = double(interp);
-matlabbatch{1}.spm.tools.cat.tools.defs2.modulate = 0;
+% matlabbatch{1}.spm.tools.cat.tools.defs2.field = {char(field_file+",1")};
+% matlabbatch{1}.spm.tools.cat.tools.defs2.images = {{char(in_file+",1")}};
+% matlabbatch{1}.spm.tools.cat.tools.defs2.interp = double(interp);
+% matlabbatch{1}.spm.tools.cat.tools.defs2.modulate = 0;
+matlabbatch{1}.spm.tools.cat.tools.defs.field1 = {char(field_file+",1")};
+matlabbatch{1}.spm.tools.cat.tools.defs.images = {char(in_file+",1")};
+matlabbatch{1}.spm.tools.cat.tools.defs.interp = double(interp);
+matlabbatch{1}.spm.tools.cat.tools.defs.modulate = 0;
 spm_jobman('run', matlabbatch);
 end
