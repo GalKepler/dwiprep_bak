c2206749a4184b655aea62d8dcb082cb5aea8054
Zvi Baratz <z.baratz@gmail.com>
2021-03-20 15:27:16 +0200

    Renamed CLI module to cli.

--- dwiprep/cli/dwiprep.py
+++ dwiprep/cli/dwiprep.py
@@ -1,6 +1,6 @@
 import argparse
 
-from dwiprep.CLI.configuration import PARSER_CONFIGRATION
+from dwiprep.cli.configuration import PARSER_CONFIGRATION
 
 parser = argparse.ArgumentParser(**PARSER_CONFIGRATION)
 parser.add_argument("-input", type=str, required=False)
