96de278c841d0e3dcb5b15778ca7be00f14ec26e
Zvi Baratz <z.baratz@gmail.com>
2021-03-22 13:11:36 +0200

    Update preprocess.py

--- dwiprep/preprocessing/preprocess.py
+++ dwiprep/preprocessing/preprocess.py
@@ -14,13 +14,14 @@ class PreprocessPipeline:
 
     def __init__(self, input_dict: dict, output_dir: Path):
         """
-        Initiate the PreprocessPipeline instance
+        Initiate the PreprocessPipeline instance.
+        
         Parameters
         ----------
         input_dict : dict
-            [Input dictionary containing "anatomical", "ap" and "pa" keys]
+            Input dictionary containing "anatomical", "ap" and "pa" keys
         output_dir : Path
-            [Path to subject's output derivatives directory]
+            Path to subject's output derivatives directory
         """
         self.longitudinal = False
         self.validate_input(input_dict)
@@ -144,7 +145,7 @@ class PreprocessPipeline:
         Parameters
         ----------
         session : str
-            key representing a session within the dataset
+            Key representing a session within the dataset
         session_dict : dict
             Dictionary containing paths to session-relevant files
         target_dir : Path
@@ -183,7 +184,7 @@ class PreprocessPipeline:
         Parameters
         ----------
         session : str
-            key representing a session within the dataset
+            Key representing a session within the dataset
         target_dir : Path
             Path to user-defined session's output directory
         """
@@ -220,7 +221,7 @@ class PreprocessPipeline:
         Parameters
         ----------
         session : str
-            key representing a session within the dataset
+            Key representing a session within the dataset
         session_dict : dict
             Dictionary containing paths to session-relevant files
         target_dir : Path
@@ -253,7 +254,7 @@ class PreprocessPipeline:
         Parameters
         ----------
         session : str
-            key representing a session within the dataset
+            Key representing a session within the dataset
         target_dir : Path
             Path to user-defined session's output directory
         """
@@ -288,7 +289,7 @@ class PreprocessPipeline:
         Parameters
         ----------
         session : str
-            key representing a session within the dataset
+            Key representing a session within the dataset
         target_dir : Path
             Path to user-defined session's output directory
         """
@@ -326,7 +327,7 @@ class PreprocessPipeline:
         Parameters
         ----------
         session : str
-            key representing a session within the dataset
+            Key representing a session within the dataset
         target_dir : Path
             Path to user-defined session's output directory
         """
@@ -403,7 +404,8 @@ class PreprocessPipeline:
 
     def run_corrections(self):
         """
-        Peform the "correction" part of the pipeline, i.e cleaning the data in native-space
+        Peform the "correction" part of the pipeline, i.e cleaning the data
+        in native-space.
         """
         for session, session_dict in self.input_dict.items():
             target_dir = self.output_dict.get(session).get("directory")
@@ -421,13 +423,16 @@ class PreprocessPipeline:
 
     def run_registrations(self, atlas: dict = None, use_matlab: bool = True):
         """
-        Apply the registration pipeline via the RegisterationPipeline instance
+        Apply the registration pipeline via the RegisterationPipeline instance.
+        
         Parameters
         ----------
         atlas : dict, optional
-            [Dictionary with "path" and "name" of a parcellation atlas in MNI space], by default None
+            Dictionary with "path" and "name" of a parcellation atlas in MNI
+            space, by default None
         use_matlab : bool, optional
-            [If True, use SPM's CAT12 anatomical preprocessing pipeline, otherwise use fsl_anat script.], by default True
+            If True, use SPM's CAT12 anatomical preprocessing pipeline, otherwise
+            use fsl_anat script, by default True
         """
         registrations_dir = self.output_dir / "registrations"
         if not registrations_dir.exists():
